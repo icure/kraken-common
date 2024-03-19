00d7ca7465922cbd05fcc45561b33e15c74416e4
Clément VANDENDAELEN <cv@icure.com>
2024-03-19 11:49:58 +0100

    Changed StateToUpdate import in State to v2

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/form/template/State.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/form/template/State.kt
@@ -2,7 +2,6 @@ package org.taktik.icure.services.external.rest.v2.dto.embed.form.template
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
-import org.taktik.icure.services.external.rest.v1.dto.embed.form.template.StateToUpdate
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
