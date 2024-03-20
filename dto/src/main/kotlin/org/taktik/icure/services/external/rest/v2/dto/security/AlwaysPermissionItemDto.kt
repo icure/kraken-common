0b53c1d66c9dfa57cc874ffc61937f68dff3efb2
Clément VANDENDAELEN <cv@icure.com>
2024-03-20 15:40:43 +0100

    AlwaysPermissionItemDto default predicate value

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/security/AlwaysPermissionItemDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/security/AlwaysPermissionItemDto.kt
@@ -2,11 +2,15 @@ package org.taktik.icure.services.external.rest.v2.dto.security
 
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import io.swagger.v3.oas.annotations.media.Schema
 import org.taktik.icure.handlers.JsonDiscriminated
 import org.taktik.icure.services.external.rest.v2.dto.filter.predicate.AlwaysPredicate
 
 @JsonDeserialize(using = JsonDeserializer.None::class)
 @JsonDiscriminated("AlwaysPermissionItemDto")
-data class AlwaysPermissionItemDto(override val type: PermissionTypeDto) : PermissionItemDto {
+data class AlwaysPermissionItemDto(
+	override val type: PermissionTypeDto
+) : PermissionItemDto {
+	@Schema(defaultValue = "AlwaysPredicate()")
 	override val predicate = AlwaysPredicate()
 }
