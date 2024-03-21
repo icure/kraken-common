a79a177a0c1475b655b0ddf224efbdf438e59d7c
Vincenzo Pierro <vcp@icure.com>
2024-03-21 18:21:17 +0100

    Reverted changes

---
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/enums/UsersStatus.kt
@@ -0,0 +1,4 @@
+package org.taktik.icure.services.external.rest.v2.dto.enums
+
+enum class UsersStatus {
+}
--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/requests/BulkShareOrUpdateMetadataParamsDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/requests/BulkShareOrUpdateMetadataParamsDto.kt
@@ -7,15 +7,17 @@ import com.fasterxml.jackson.annotation.JsonInclude
 @JsonIgnoreProperties(ignoreUnknown = false)
 data class BulkShareOrUpdateMetadataParamsDto(
     val requestsByEntityId: Map<String, EntityRequestInformationDto>
-)
+) {
 
-@JsonInclude(JsonInclude.Include.NON_DEFAULT)
-@JsonIgnoreProperties(ignoreUnknown = false)
-data class EntityRequestInformationDto(
-    val requests: Map<String, EntityShareOrMetadataUpdateRequestDto>,
-    /**
-     * Which delegations can be parents to any newly requested non-root delegations. Some may be ignored in order to
-     * simplify the delegation graph, or if the requested permission is root.
-     */
-    val potentialParentDelegations: Set<String>
-)
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    @JsonIgnoreProperties(ignoreUnknown = false)
+    data class EntityRequestInformationDto(
+        val requests: Map<String, EntityShareOrMetadataUpdateRequestDto>,
+        /**
+         * Which delegations can be parents to any newly requested non-root delegations. Some may be ignored in order to
+         * simplify the delegation graph, or if the requested permission is root.
+         */
+        val potentialParentDelegations: Set<String>
+    )
+
+}
