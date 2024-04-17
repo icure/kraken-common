627659387297cb2d28b507ba741dde1796f2b086
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2024-04-17 14:06:42 +0200

    Updated warmup strategy on Patient-DataOwner (#38)

--- core/src/main/kotlin/org/taktik/icure/asyncdao/impl/PatientDAOImpl.kt
+++ core/src/main/kotlin/org/taktik/icure/asyncdao/impl/PatientDAOImpl.kt
@@ -825,7 +825,14 @@ class PatientDAOImpl(
 
 	override suspend fun warmupPartition(datastoreInformation: IDatastoreInformation, partition: Partitions) {
 		when(partition) {
-			Partitions.DataOwner -> warmup(datastoreInformation, "by_data_owner_ssin" to DATA_OWNER_PARTITION)
+			Partitions.DataOwner -> {
+				val client = couchDbDispatcher.getClient(datastoreInformation)
+				val viewQueries = createQueries(
+					datastoreInformation,
+					"by_data_owner_ssin" to DATA_OWNER_PARTITION
+				).doNotIncludeDocs()
+				client.interleave<Array<String>, String>(viewQueries, compareBy {it[0]}).firstOrNull()
+			}
 			Partitions.Maurice -> {
 				val client = couchDbDispatcher.getClient(datastoreInformation)
 				val viewQueries = createQueries(
