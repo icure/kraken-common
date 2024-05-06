34543682386103c634ee0b89bb256b65cafdda8c
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2024-05-06 15:55:14 +0200

    Generation/add always encryptable on dto (#45)
    * Added AlwaysEncryptable for generation
    
    * Small fixes on dtos

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/RecoveryDataDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/RecoveryDataDto.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.taktik.icure.services.external.rest.v2.dto.base.StoredDocumentDto
 import org.taktik.icure.services.external.rest.v2.dto.base.VersionableDto
+import org.taktik.icure.services.external.rest.v2.dto.specializations.Base64StringDto
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -11,7 +12,7 @@ data class RecoveryDataDto(
     override val id: String,
     override val rev: String? = null,
     val recipient: String,
-    val encryptedSelf: String,
+    val encryptedSelf: Base64StringDto,
     val type: Type,
     val expirationInstant: Long? = null,
     override val deletionDate: Long? = null
