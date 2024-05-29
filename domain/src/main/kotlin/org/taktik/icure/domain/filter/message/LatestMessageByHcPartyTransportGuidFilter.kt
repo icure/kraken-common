da477b26299a0e787ebe66bed99e376571c2ad6d
Antoine Duchâteau <ad@icure.com>
2024-05-29 15:09:09 +0200

    Delete filters from common (#57)

--- domain/src/main/kotlin/org/taktik/icure/domain/filter/message/LatestMessageByHcPartyTransportGuidFilter.kt
+++
@@ -1,9 +0,0 @@
-package org.taktik.icure.domain.filter.message
-
-import org.taktik.icure.domain.filter.Filter
-import org.taktik.icure.entities.Message
-
-interface LatestMessageByHcPartyTransportGuidFilter : Filter<String, Message> {
-    val healthcarePartyId: String
-    val transportGuid: String
-}
