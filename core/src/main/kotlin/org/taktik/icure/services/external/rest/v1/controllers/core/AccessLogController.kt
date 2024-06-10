e9fee50f2a8217f507b3d5a61a0b003f9baa4192
Vincenzo Pierro <vcp@icure.com>
2024-06-10 10:18:04 +0200

    CSM-360 made the date range on access log more lenient

--- core/src/main/kotlin/org/taktik/icure/services/external/rest/v1/controllers/core/AccessLogController.kt
+++ core/src/main/kotlin/org/taktik/icure/services/external/rest/v1/controllers/core/AccessLogController.kt
@@ -89,12 +89,20 @@ class AccessLogController(
 		@RequestParam(required = false) descending: Boolean?
 	): PaginatedFlux<AccessLogDto> {
 		val paginationOffset = PaginationOffset(startKey, startDocumentId, null, limit ?: paginationConfig.defaultLimit)
-		return accessLogService.listAccessLogsBy(
-			if (descending == true) toEpoch ?: Long.MAX_VALUE else fromEpoch ?: 0,
-			if (descending == true) fromEpoch ?: 0 else toEpoch ?: Long.MAX_VALUE,
-			paginationOffset,
-			descending == true
-		).mapElements(accessLogMapper::map).asPaginatedFlux()
+
+		val (from, to) = when {
+			descending == true && fromEpoch != null && toEpoch != null && fromEpoch >= toEpoch -> fromEpoch to toEpoch
+			descending == true && fromEpoch != null && toEpoch != null && fromEpoch < toEpoch -> toEpoch to fromEpoch
+			fromEpoch != null && toEpoch != null && fromEpoch >= toEpoch -> toEpoch to fromEpoch
+			fromEpoch != null && toEpoch != null && fromEpoch < toEpoch -> fromEpoch to toEpoch
+			descending == true -> (toEpoch ?: Long.MAX_VALUE) to (fromEpoch ?: 0)
+			else -> (fromEpoch ?: 0) to (toEpoch ?: Long.MAX_VALUE)
+		}
+
+		return accessLogService
+			.listAccessLogsBy(from, to, paginationOffset, descending == true)
+			.mapElements(accessLogMapper::map)
+			.asPaginatedFlux()
 	}
 
 	@Operation(summary = "Get paginated list of Access Logs")
