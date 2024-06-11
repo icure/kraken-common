f7e9b0eb4c8e82306725e3f4a1f17c52b093a60e
Vincenzo Pierro <vcp@icure.com>
2024-06-11 10:37:28 +0200

    wip

--- core/src/main/kotlin/org/taktik/icure/asyncdao/components/ExternalViewLoader.kt
+++ core/src/main/kotlin/org/taktik/icure/asyncdao/components/ExternalViewLoader.kt
@@ -2,22 +2,22 @@ package org.taktik.icure.asyncdao.components
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.treeToValue
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import org.bouncycastle.crypto.signers.Ed25519Signer
 import org.bouncycastle.crypto.util.OpenSSHPublicKeyUtil
+import org.taktik.couchdb.entity.View
 import org.taktik.couchdb.support.views.ExternalViewRepository
 import java.security.MessageDigest
+import java.time.Duration
 import java.util.*
-import kotlin.math.sign
 
 abstract class ExternalViewLoader(
 	private val objectMapper: ObjectMapper,
@@ -30,6 +30,10 @@ abstract class ExternalViewLoader(
 			val signature: String
 		)
 	}
+
+	private val manifestCache = Caffeine.newBuilder()
+		.expireAfterWrite(Duration.ofMinutes(10))
+		.build<String, Map<String, String>>()
 
 	private val publicSigningKey = rawPublicSigningKey.split(" ")[1].let {
 		OpenSSHPublicKeyUtil.parsePublicKey(Base64.getDecoder().decode(it))
@@ -61,21 +65,22 @@ abstract class ExternalViewLoader(
 		check(verifySignature(signedContent)) {
 			"Cannot verify the signature for the manifest"
 		}
-		return objectMapper.readValue<T>(signedContent.content)
+		return objectMapper.treeToValue(signedContent.content)
 	}
 
-	private suspend fun downloadManifest(repoUrl: String): Map<String, ResourceWithSignature> {
-		val (manifestUrl, signatureUrl) = gitHubRawUrlForResource(repoUrl, manifestResource)
+	private suspend fun getOrDownloadManifest(repoUrl: String): Map<String, String> = manifestCache.getIfPresent(repoUrl) ?:
+		downloadAndVerifyResource<Map<String, String>>(repoUrl, "output/manifest.json").also {
+			manifestCache.put(repoUrl, it)
+		}
 
-		val rawManifest = httpClient.get(manifestUrl).bodyAsText()
-		val rawSignature = httpClient.get(signatureUrl).bodyAsText()
-
-
-
-		return objectMapper.readValue<Map<String, ResourceWithSignature>>(rawManifest)
-	}
-
-
-	abstract suspend fun loadExternalViews(entityClass: Class<*>, partition: String): ExternalViewRepository
+	protected suspend fun loadExternalViews(repoUrl: String, entityClass: Class<*>, partition: String): ExternalViewRepository? =
+		getOrDownloadManifest(repoUrl)[entityClass.simpleName.lowercase()]?.let { viewUrl ->
+			val views = downloadAndVerifyResource<Map<String, View>>(repoUrl, viewUrl)
+			ExternalViewRepository(
+				secondaryPartition = partition,
+				klass = entityClass,
+				views = views
+			)
+		}
 
 }
