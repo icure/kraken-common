42ce65500eea6aad4bc12634772dcf30c089f730
Antoine Duchâteau <ad@icure.com>
2024-07-05 14:49:33 +0200

    Remove sam views (#65)

--- dao/src/main/resources/org/taktik/icure/asyncdao/samv2/impl/js/verse/By_chapter_paragraph.js
+++
@@ -1,5 +0,0 @@
-map = function(doc) {
-    if (doc.java_type === 'org.taktik.icure.entities.samv2.Verse' && !doc.deleted) {
-        emit([doc.chapterName, doc.paragraphName, doc.verseNum, doc.verseSeq], 1)
-    }
-};
--- dao/src/main/resources/org/taktik/icure/asyncdao/samv2/impl/js/vmp/By_groupid.js
+++
@@ -1,5 +0,0 @@
-map = function(doc) {
-    if (doc.java_type === 'org.taktik.icure.entities.samv2.Vmp' && !doc.deleted && doc.vmpGroup && doc.vmpGroup.id) {
-        emit(doc.vmpGroup.id, 1)
-    }
-};
