1045acc7031caeabda04b92474719b738d6aa8dc
Clément VANDENDAELEN <cv@icure.com>
2024-07-11 11:17:32 +0200

    Revert "feat: Added diseaseCodes to MedicinalProduct"
    This reverts commit 7d41b1b6d8753970dd972462645e1b21fef9542b.

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/MedicinalproductDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/MedicinalproductDto.kt
@@ -20,7 +20,6 @@ package org.taktik.icure.services.external.rest.v2.dto.embed
 import java.io.Serializable
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
-import io.swagger.v3.oas.annotations.media.Schema
 import org.taktik.icure.services.external.rest.v2.dto.base.CodeStubDto
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -30,6 +29,5 @@ data class MedicinalproductDto(
 	val deliveredcds: List<CodeStubDto> = emptyList(),
 	val intendedname: String? = null,
 	val deliveredname: String? = null,
-	val productId: String? = null,
-	@Schema(description = "Codes that indicates the diseases or conditions that the medicinal product is intended to treat, cure, or vaccinate against") val diseaseCodes: List<CodeStubDto> = emptyList()
+	val productId: String? = null
 ) : Serializable
