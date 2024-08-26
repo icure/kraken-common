6ff185099069f6e4ddc6be3a12e7d9a000e67cc7
Vincenzo Pierro <vcp@icure.com>
2024-08-26 09:19:36 +0200

    Added AllAgendasFilter

---
+++ core/src/main/kotlin/org/taktik/icure/asynclogic/impl/filter/agenda/AllAgendasFilter.kt
@@ -0,0 +1,22 @@
+package org.taktik.icure.asynclogic.impl.filter.agenda
+
+import kotlinx.coroutines.flow.Flow
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+import org.taktik.icure.asyncdao.AgendaDAO
+import org.taktik.icure.asynclogic.datastore.IDatastoreInformation
+import org.taktik.icure.asynclogic.impl.filter.Filter
+import org.taktik.icure.domain.filter.Filters
+import org.taktik.icure.entities.Agenda
+
+@Service
+@Profile("app")
+class AllAgendasFilter(
+	private val agendaDAO: AgendaDAO
+) : Filter<String, Agenda, Filters.AllFilter<String, Agenda>> {
+	override fun resolve(
+		filter: Filters.AllFilter<String, Agenda>,
+		context: org.taktik.icure.asynclogic.impl.filter.Filters,
+		datastoreInformation: IDatastoreInformation
+	): Flow<String> = agendaDAO.getEntityIds(datastoreInformation)
+}
---
+++ domain/src/main/kotlin/org/taktik/icure/domain/filter/impl/agenda/AllAgendasFilter.kt
@@ -0,0 +1,17 @@
+package org.taktik.icure.domain.filter.impl.agenda
+
+import org.taktik.icure.domain.filter.AbstractFilter
+import org.taktik.icure.domain.filter.Filters
+import org.taktik.icure.entities.Agenda
+import org.taktik.icure.entities.base.HasEncryptionMetadata
+
+data class AllAgendasFilter(
+	override val desc: String? = null,
+) : AbstractFilter<Agenda>, Filters.AllFilter<String, Agenda> {
+
+	override val canBeUsedInWebsocket = true
+	override val requiresSecurityPrecondition: Boolean = false
+	override fun requestedDataOwnerIds(): Set<String> = emptySet()
+
+	override fun matches(item: Agenda, searchKeyMatcher: (String, HasEncryptionMetadata) -> Boolean) = true
+}
