9a0acd8ca84df4e51a9c3203ae346f9e87a23ca0
Vincenzo Pierro <vcp@icure.com>
2025-01-27 16:24:32 +0100

    Fixed InternalDAO

--- dao/src/main/kotlin/org/taktik/icure/asyncdao/InternalDAO.kt
+++ dao/src/main/kotlin/org/taktik/icure/asyncdao/InternalDAO.kt
@@ -23,6 +23,7 @@ import org.taktik.couchdb.BulkUpdateResult
 import org.taktik.couchdb.DocIdentifier
 import org.taktik.couchdb.entity.Option
 import org.taktik.couchdb.id.Identifiable
+import org.taktik.icure.asyncdao.results.BulkSaveResult
 
 interface InternalDAO<T : Identifiable<String>> {
 
@@ -32,10 +33,10 @@ interface InternalDAO<T : Identifiable<String>> {
 	suspend fun get(id: String, rev: String?, vararg options: Option): T?
 	fun getEntities(ids: Collection<String>): Flow<T>
 	suspend fun save(entity: T): T?
-	fun save(entities: Flow<T>): Flow<DocIdentifier>
+	fun save(entities: Flow<T>): Flow<BulkSaveResult<T>>
 	suspend fun update(entity: T): T?
 
-	fun save(entities: List<T>): Flow<DocIdentifier>
+	fun save(entities: List<T>): Flow<BulkSaveResult<T>>
 	suspend fun purge(entity: T): DocIdentifier
 	fun purge(entities: Flow<T>): Flow<BulkUpdateResult>
 	suspend fun remove(entity: T): DocIdentifier
