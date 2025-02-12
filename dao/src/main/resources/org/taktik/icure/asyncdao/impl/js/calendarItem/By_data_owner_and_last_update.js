be79a956bd3dc343481cae1bf80dd78ddd900c09
Vincenzo Claudio Pierro <vcp@icure.com>
2025-02-12 11:57:56 +0000

    Update dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/calendarItem/By_data_owner_and_last_update.js
    Co-authored-by: Antoine Duchâteau <ad@icure.com>

--- dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/calendarItem/By_data_owner_and_last_update.js
+++ dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/calendarItem/By_data_owner_and_last_update.js
@@ -44,7 +44,7 @@ function(doc) {
     if (doc.java_type === 'org.taktik.icure.entities.CalendarItem' && (doc.deleted != null || doc.created != null || doc.modified != null)) {
         const latestUpdate = [doc.deleted, doc.created, doc.modified].reduce((p, c) => (c ? c : 0) > p ? c : p, 0)
         emit_for_delegates(doc, function (dataOwnerId, doc) {
-            emit([dataOwnerId, latestUpdate], doc._id)
+            emit([dataOwnerId, latestUpdate], null)
         })
     }
 }
