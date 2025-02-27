58220d590442c6046e47cf420ffbad6c06d0f12e
aduchate <ad@icure.com>
2025-02-27 09:44:56 +0100

    Better contact modification view

--- dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/contact/By_data_owner_and_last_update.js
+++ dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/contact/By_data_owner_and_last_update.js
@@ -17,34 +17,38 @@ function(doc) {
                 }
             }
             if (metadata.secureDelegations) {
+                const responsibleId = Object.entries(metadata.secureDelegations).reduce((acc, [, sd]) => acc ? acc : sd.delegator === sd.delegate ? sd.delegator : null , null)
+
                 for (const [delegationKey, secureDelegation] of Object.entries(metadata.secureDelegations)) {
                     if (secureDelegation.delegate) {
                         if (!emittedDataOwners.has(secureDelegation.delegate)) {
                             emittedDataOwners.add(secureDelegation.delegate)
-                            emitWithDelegateAndDoc(secureDelegation.delegate, doc)
+                            emitWithDelegateAndDoc(secureDelegation.delegate, responsibleId, doc)
                         }
                     }
                     if (secureDelegation.delegator) {
                         if (!emittedDataOwners.has(secureDelegation.delegator)) {
                             emittedDataOwners.add(secureDelegation.delegator)
-                            emitWithDelegateAndDoc(secureDelegation.delegator, doc)
+                            emitWithDelegateAndDoc(secureDelegation.delegator, responsibleId, doc)
                         }
                     }
                     if (!secureDelegation.delegate || !secureDelegation.delegator) {
-                        emitWithDelegateAndDoc(delegationKey, doc)
+                        emitWithDelegateAndDoc(delegationKey, responsibleId, doc)
                         const equivalences = equivalencesByCanonical[delegationKey]
                         if (equivalences) {
-                            equivalences.forEach(function (equivalence) { emitWithDelegateAndDoc(equivalence, doc) })
+                            equivalences.forEach(function (equivalence) { emitWithDelegateAndDoc(equivalence, responsibleId, doc) })
                         }
                     }
                 }
             }
         }
         if (doc.delegations) {
+            const responsibleId = Object.entries(doc.delegations).reduce((acc, [, sd]) => acc ? acc : sd.reduce((acc, sd) => sd.owner === sd.delegatedTo ? sd.owner : null, null )  , null)
+
             Object.keys(doc.delegations).forEach(function (k) {
                 if(!emittedDataOwners.has(k)) {
                     // No reason to do add: keys from doc.delegations can't be duplicated, and doc.delegations are the last thing we emit
-                    emitWithDelegateAndDoc(k, doc)
+                    emitWithDelegateAndDoc(k, responsibleId, doc)
                 }
             });
         }
@@ -52,8 +56,8 @@ function(doc) {
 
     if (doc.java_type === 'org.taktik.icure.entities.CalendarItem' && (doc.deleted != null || doc.created != null || doc.modified != null)) {
         const latestUpdate = [doc.deleted, doc.created, doc.modified].reduce((p, c) => (c ? c : 0) > p ? c : p, 0)
-        emit_for_delegates(doc, function (dataOwnerId, doc) {
-            emit([dataOwnerId, latestUpdate], null)
+        emit_for_delegates(doc, function (dataOwnerId, responsibleId) {
+            emit([dataOwnerId, latestUpdate], responsibleId)
         })
     }
 }
