8a39a11b1651f26f08b0af1ca621ffc9f4d8c84d
Luca Tremamunno <tremaluca96@gmail.com>
2025-04-10 11:00:06 +0200

    Revert changes to Agenda/ReadableByUser view

--- dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/agenda/Readable_by_user.js
+++ dao/src/main/resources/org/taktik/icure/asyncdao/impl/js/agenda/Readable_by_user.js
@@ -1,17 +1,7 @@
 function(doc) {
     if (doc.java_type === 'org.taktik.icure.entities.Agenda' && !doc.deleted) {
-        const emittedUsers = new Set();
         doc.rights.forEach(function(right) {
-            if (!emittedUsers.has(right)) {
-                emittedUsers.add(right);
-                emit(right.userId, null);
-            }
+            emit(right.userId, null);
         });
-        Object.keys(doc.userRights).forEach(function(userId) {
-            if (!emittedUsers.has(userId)) {
-                emittedUsers.add(userId);
-                emit(userId, null);
-            }
-        })
     }
 }
