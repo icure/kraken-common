ddf4ff72cd7194a2288e65f911cc69fcfedf4ec0
Vincenzo Pierro <vcp@icure.com>
2025-04-11 08:44:12 +0200

    missing dependency in mapper

--- mapper/src/main/kotlin/org/taktik/icure/services/external/rest/v2/mapper/embed/EmbeddedTimeTableMapper.kt
+++ mapper/src/main/kotlin/org/taktik/icure/services/external/rest/v2/mapper/embed/EmbeddedTimeTableMapper.kt
@@ -4,8 +4,9 @@ import org.mapstruct.InjectionStrategy
 import org.mapstruct.Mapper
 import org.taktik.icure.entities.embed.EmbeddedTimeTable
 import org.taktik.icure.services.external.rest.v2.dto.embed.EmbeddedTimeTableDto
+import org.taktik.icure.services.external.rest.v2.mapper.base.CodeStubV2Mapper
 
-@Mapper(componentModel = "spring", uses = [TimeTableItemV2Mapper::class], injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = "spring", uses = [TimeTableItemV2Mapper::class, CodeStubV2Mapper::class], injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 interface EmbeddedTimeTableV2Mapper {
 	fun map(embeddedTimeTableDto: EmbeddedTimeTableDto): EmbeddedTimeTable
 	fun map(embeddedTimeTable: EmbeddedTimeTable): EmbeddedTimeTableDto
