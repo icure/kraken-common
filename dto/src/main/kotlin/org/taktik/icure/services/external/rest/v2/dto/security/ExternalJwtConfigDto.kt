25349f3ab3d94da0b91b5a9ea8fd30f29cb9a77c
Luca Tremamunno <tremaluca96@gmail.com>
2025-05-08 18:14:23 +0200

    Update external jwt config to enforce exactly 1 field selector

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/security/ExternalJwtConfigDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/security/ExternalJwtConfigDto.kt
@@ -9,12 +9,10 @@ import org.taktik.icure.services.external.rest.v2.dto.embed.AuthenticationClassD
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ExternalJwtConfigDto(
+	@Schema(description = "Specifies how the external jwt should be validated at login")
 	val validationMethod: ValidationMethodDto,
-	@Schema(description = "The name of the field that contains the icure user local id in the external JWT.") val idField: String? = null,
-	@Schema(description = "The name of the field that contains the email in the external JWT.") val emailField: String? = null,
-	@Schema(description = "The name of the field that contains the login in the external JWT.") val loginField: String? = null,
-	@Schema(description = "The name of the field that contains the phone number in the external JWT.") val mobilePhoneField: String? = null,
-	@Schema(description = "The assigner of an identifier plus the name of the field that contains the value of the identifier.") val identifiersFields: Set<IdentifierSelectorDto> = emptySet(),
+	@Schema(description = "Specifies which field of the external jwt should be used to find the matching users")
+	val fieldSelector: FieldSelectorDto,
 	@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 	@Schema(description = "The authentication class assigned to the JWT of users that login using this configuration, default is AuthenticationClass.EXTERNAL_AUTHENTICATION")
 	val authenticationClass: AuthenticationClassDto = AuthenticationClassDto.EXTERNAL_AUTHENTICATION,
@@ -23,5 +21,14 @@ data class ExternalJwtConfigDto(
 	sealed interface ValidationMethodDto {
 		data class PublicKey(val key: String): ValidationMethodDto
 		data class Oidc(val issureLocation: String): ValidationMethodDto
+	}
+
+	@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, property = "type")
+	sealed interface FieldSelectorDto {
+		data class LocalId(val fieldName: String) : FieldSelectorDto
+		data class Email(val fieldName: String) : FieldSelectorDto
+		data class MobilePhone(val fieldName: String) : FieldSelectorDto
+		data class Username(val fieldName: String) : FieldSelectorDto
+		data class Identifier(val identifierAssigner: String, val fieldName: String) : FieldSelectorDto
 	}
 }
