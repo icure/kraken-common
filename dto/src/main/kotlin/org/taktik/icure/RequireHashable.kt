6cc0dc231991000dddb8c9955e5f22fdb3356c17
Luca Tremamunno <tremaluca96@hotmail.it>
2025-06-02 09:56:44 +0200

    New require hashable annotation (#190)

---
+++ dto/src/main/kotlin/org/taktik/icure/RequireHashable.kt
@@ -0,0 +1,9 @@
+package org.taktik.icure
+
+/**
+ * This annotation is only used for the generation of entities for the multiplatform.
+ * Refer to com.icure.cardinal.sdk.utils.generation.RequireHashable in cardinal-multiplatform-sdk for more info
+ */
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS)
+annotation class RequireHashable
