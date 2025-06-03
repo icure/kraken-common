fcda97d91c89fe009697a3d199118a1e8ccd9195
Vincenzo Pierro <vcp@icure.com>
2025-06-03 14:57:38 +0200

    Added MessageByDataOwnerLifeCycle filter

---
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/filter/message/MessageByDataOwnerLifecycleBetween.kt
@@ -0,0 +1,21 @@
+package org.taktik.icure.services.external.rest.v2.dto.filter.message
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import org.taktik.icure.handlers.JsonPolymorphismRoot
+import org.taktik.icure.services.external.rest.v2.dto.MessageDto
+import org.taktik.icure.services.external.rest.v2.dto.filter.AbstractFilterDto
+
+@JsonPolymorphismRoot(AbstractFilterDto::class)
+@JsonDeserialize(using = JsonDeserializer.None::class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class MessageByDataOwnerLifecycleBetween(
+	val dataOwnerId: String,
+	val startTimestamp: Long? = null,
+	val endTimestamp: Long? = null,
+	val descending: Boolean = false,
+	override val desc: String? = null
+) : AbstractFilterDto<MessageDto>
