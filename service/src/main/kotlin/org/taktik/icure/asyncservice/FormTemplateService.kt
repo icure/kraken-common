2a2aa90c31fb0c55d1ec5f3f61c4c948f26c9bec
Antoine Duchâteau <ad@icure.com>
2025-07-09 10:50:43 +0200

    Api analysis fixes (#205)
    * Block anonymous data owners from accessing occurrences methods (#144)
    
    * Deprecate + safer implementation getFormTemplatesByGuid (#148)
    
    ---------
    
    Co-authored-by: Luca Tremamunno <tremaluca96@hotmail.it>

--- service/src/main/kotlin/org/taktik/icure/asyncservice/FormTemplateService.kt
+++ service/src/main/kotlin/org/taktik/icure/asyncservice/FormTemplateService.kt
@@ -11,6 +11,13 @@ interface FormTemplateService {
 	fun createFormTemplates(entities: Collection<FormTemplate>, createdEntities: Collection<FormTemplate>): Flow<FormTemplate>
 	suspend fun createFormTemplate(entity: FormTemplate): FormTemplate
 	suspend fun getFormTemplate(formTemplateId: String): FormTemplate?
+	/**
+	 * If there is any form template with author=[userId] and guid=[formTemplateGuid] returns them, regardless of
+	 * [specialityCode].
+	 * Else returns all form templates with specialty.code=[specialityCode] and guid=[formTemplateGuid] regardless of
+	 * [userId].
+	 */
+	@Deprecated("This method has unintuitive behaviour, read FormTemplateService.getFormTemplatesByGuid doc for more info")
 	fun getFormTemplatesByGuid(userId: String, specialityCode: String, formTemplateGuid: String): Flow<FormTemplate>
 	fun getFormTemplatesBySpecialty(specialityCode: String, loadLayout: Boolean): Flow<FormTemplate>
 	fun getFormTemplatesByUser(userId: String, loadLayout: Boolean): Flow<FormTemplate>
