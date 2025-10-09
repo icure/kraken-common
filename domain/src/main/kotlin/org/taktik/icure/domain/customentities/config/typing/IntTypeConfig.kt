2dfe897a344210ea3675f0522eb24da0d641a77a
Luca Tremamunno <tremaluca96@gmail.com>
2025-10-09 16:47:43 +0200

    Always enforce limits of int config within js safe ranges

--- domain/src/main/kotlin/org/taktik/icure/domain/customentities/config/typing/IntTypeConfig.kt
+++ domain/src/main/kotlin/org/taktik/icure/domain/customentities/config/typing/IntTypeConfig.kt
@@ -11,12 +11,26 @@ data class IntTypeConfig(
 	val nullable: Boolean = false,
 	val validation: ValidationConfig? = null
 ) : GenericTypeConfig {
+	companion object {
+		/**
+		 * Biggest long that we can safely store without incurring in any issues with js numbers or couchdb views
+		 */
+		private const val MAX_SAFE_LONG = 9007199254740991
+		/**
+		 * Smallest long that we can safely store without incurring in any issues with js numbers or couchdb views
+		 */
+		private const val MIN_SAFE_LONG = -9007199254740991
+	}
 
 	data class ValidationConfig(
+		/**
+		 * Minimum acceptable value for the , always inclusive
+		 */
 		val min: Long? = null,
+		/**
+		 * Maximum acceptable value for the , always inclusive
+		 */
 		val max: Long? = null,
-		val exclusiveMin: Boolean = false,
-		val exclusiveMax: Boolean = false
 	)
 
 	override fun validateConfig(
@@ -26,6 +40,15 @@ data class IntTypeConfig(
 		validation?.apply {
 			require(min != null || max != null) {
 				"$path: invalid int validation config, must specify a lower and/or upper limit"
+			}
+			require(min == null || min in MIN_SAFE_LONG..MAX_SAFE_LONG) {
+				"$path: invalid int validation config, value for `min` is not within the safe range for JS integers"
+			}
+			require(max == null || max in MIN_SAFE_LONG..MAX_SAFE_LONG) {
+				"$path: invalid int validation config, value for `max` is not within the safe range for JS integers"
+			}
+			require(max == null || min == null || max > min) {
+				"$path: invalid int validation config, value for `max` should be greater than value for `min`"
 			}
 		}
 	}
@@ -38,14 +61,11 @@ data class IntTypeConfig(
 		require(value is IntNode || value is LongNode) {
 			"$path: invalid type, expected Int64"
 		}
-		if (validation != null) {
-			val valueLong = value.asLong()
-			require (
-				validation.min?.let { min -> if (validation.exclusiveMin) valueLong > min else valueLong >= min } ?: true
-					&& validation.max?.let { max -> if (validation.exclusiveMax) valueLong < max else valueLong <= max } ?: true
-			) {
-				"$path: value $valueLong out of configured bounds"
-			}
+		val valueLong = value.asLong()
+		require (
+			valueLong >= (validation?.min ?: MIN_SAFE_LONG) && valueLong <= (validation?.max ?: MAX_SAFE_LONG)
+		) {
+			"$path: value $valueLong out of configured bounds"
 		}
 		value
 	}
