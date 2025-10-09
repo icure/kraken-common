991114de471cc91173fffd8d0bafb47f60b5b1fe
Antoine Duchâteau <ad@icure.com>
2025-10-09 13:39:14 +0200

    Remove group aware methods from common for pricing (#234)

--- service/src/main/kotlin/org/taktik/icure/asyncservice/PricingService.kt
+++ service/src/main/kotlin/org/taktik/icure/asyncservice/PricingService.kt
@@ -17,14 +17,10 @@ interface PricingService {
 	suspend fun getTarification(id: String): Tarification?
 	suspend fun getTarification(type: String, tarification: String, version: String): Tarification?
 	fun getTarifications(ids: List<String>): Flow<Tarification>
-	fun getTarifications(groupId: String, ids: List<String>): Flow<Tarification>
 	suspend fun createTarification(tarification: Tarification): Tarification?
 	suspend fun modifyTarification(tarification: Tarification): Tarification?
 	fun findTarificationsBy(type: String?, tarification: String?, version: String?): Flow<Tarification>
 	fun findTarificationsBy(region: String?, type: String?, tarification: String?, version: String?): Flow<Tarification>
-
-	fun create(groupId: String, batch: List<Tarification>): Flow<Tarification>
-	fun modify(groupId: String, batch: List<Tarification>): Flow<Tarification>
 
 	fun matchTarificationsBy(filter: AbstractFilter<Tarification>): Flow<String>
 
@@ -72,5 +68,4 @@ interface PricingService {
 	fun findTarificationsOfTypesByLabel(region: String?, language: String?, label: String?, types: Set<String>?, paginationOffset: PaginationOffset<ComplexKey>): Flow<PaginationElement>
 	fun findTarificationsByLabel(region: String?, language: String?, type: String?, label: String?, paginationOffset: PaginationOffset<List<String?>>): Flow<ViewQueryResultEvent>
 	suspend fun getOrCreateTarification(type: String, tarification: String): Tarification?
-	fun matchTarificationsInGroupBy(filter: AbstractFilter<Tarification>, groupId: String): Flow<String>
 }
