9e30236eb4bd1a6b89d39424f0008cbea44ebf1a
Clément VANDENDAELEN <cv@icure.com>
2025-10-14 15:36:55 +0200

    refactor: Rename stockLocationDto to stockLocation and update mappers

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/MedicationDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/MedicationDto.kt
@@ -71,7 +71,7 @@ data class MedicationDto(
 	val suspension: List<SuspensionDto>? = null,
 	val prescriptionRID: String? = null,
 	val status: Int? = null,
-	val stockLocationDto: AddressDto? = null,
+	val stockLocation: AddressDto? = null,
 ) : Serializable {
 	companion object {
 		const val REIMBURSED = "REIMBURSED"
