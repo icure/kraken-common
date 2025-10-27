b0a98ce6194a8eea046aa07225de740d54171fe7
Vincenzo Claudio Pierro <vcp@icure.com>
2025-10-27 15:26:10 +0100

    Feature/add properties to patient healthcareparty (#240)
    * Added properties to PatientHealthcareParties
    
    * Updated DTOs and mappers

--- mapper/src/main/kotlin/org/taktik/icure/services/external/rest/v1/mapper/embed/PatientHealthCarePartyMapper.kt
+++ mapper/src/main/kotlin/org/taktik/icure/services/external/rest/v1/mapper/embed/PatientHealthCarePartyMapper.kt
@@ -6,11 +6,16 @@ package org.taktik.icure.services.external.rest.v1.mapper.embed
 
 import org.mapstruct.InjectionStrategy
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 import org.taktik.icure.entities.embed.PatientHealthCareParty
 import org.taktik.icure.services.external.rest.v1.dto.embed.PatientHealthCarePartyDto
 
 @Mapper(componentModel = "spring", uses = [ReferralPeriodMapper::class], injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 interface PatientHealthCarePartyMapper {
+	@Mappings(
+		Mapping(target = "properties", ignore = true),
+	)
 	fun map(patientHealthCarePartyDto: PatientHealthCarePartyDto): PatientHealthCareParty
 	fun map(patientHealthCareParty: PatientHealthCareParty): PatientHealthCarePartyDto
 }
