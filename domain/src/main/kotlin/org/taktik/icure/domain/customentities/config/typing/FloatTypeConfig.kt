9a1e6fc481357e20fc237d0b465af28d4f86b46f
Luca Tremamunno <tremaluca96@gmail.com>
2026-01-27 12:14:01 +0100

    Fix float type config validation

--- domain/src/main/kotlin/org/taktik/icure/domain/customentities/config/typing/FloatTypeConfig.kt
+++ domain/src/main/kotlin/org/taktik/icure/domain/customentities/config/typing/FloatTypeConfig.kt
@@ -37,18 +37,23 @@ data class FloatTypeConfig(
 		validationContext: ScopedErrorCollector,
 	) {
 		validation?.apply {
-			if ((max ?: Double.MAX_VALUE) <= (min ?: Double.MIN_VALUE)) {
+			if (validation.min != null && !validation.min.isFinite()) {
 				validationContext.addError("Invalid float validation config, value for `min` must be a finite float")
 			}
 			if (validation.max != null && !validation.max.isFinite()) {
 				validationContext.addError("Invalid float validation config, value for `max` must be a finite float")
 			}
-			if (validation.min != null && validation.max != null && validation.min >= validation.max) {
-				validationContext.addError("Invalid float validation config, value for `min` must be less than value for `max`")
+			if (validation.min != null && validation.max != null) {
+				if (validation.min > validation.max || (validation.min == validation.max && (validation.exclusiveMin || validation.exclusiveMax))) {
+					validationContext.addError("Invalid float validation config, range has no valid values")
+				}
 			}
-			if ((max ?: Double.MAX_VALUE) <= (min ?: -Double.MAX_VALUE)) validationContext.addError(
-				"Invalid float validation config, value for `max` should be greater than value for `min`"
-			)
+			if (validation.min == -Double.MAX_VALUE && !validation.exclusiveMin) {
+				validationContext.addWarning("Redundant float validation config, inclusive `min` matches the minimum allowed value")
+			}
+			if (validation.max == Double.MAX_VALUE && !validation.exclusiveMax) {
+				validationContext.addWarning("Redundant float validation config, inclusive `max` matches the maximum allowed value")
+			}
 		}
 	}
 
