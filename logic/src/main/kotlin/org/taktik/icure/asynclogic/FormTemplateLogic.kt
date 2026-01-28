b3758732932b3f78a25b17a5cf867d9aff571ef2
Antoine Duchâteau <ad@icure.com>
2026-01-28 16:56:05 +0100

    Add form template inGroup endpoints (#270)

--- logic/src/main/kotlin/org/taktik/icure/asynclogic/FormTemplateLogic.kt
+++ logic/src/main/kotlin/org/taktik/icure/asynclogic/FormTemplateLogic.kt
@@ -10,14 +10,8 @@ import org.taktik.icure.entities.FormTemplate
 interface FormTemplateLogic : EntityPersister<FormTemplate> {
 	fun createFormTemplates(entities: Collection<FormTemplate>, createdEntities: Collection<FormTemplate>): Flow<FormTemplate>
 
-	suspend fun createFormTemplate(entity: FormTemplate): FormTemplate
-
-	suspend fun getFormTemplate(formTemplateId: String): FormTemplate?
-
 	@Deprecated("This method has unintuitive behaviour, read FormTemplateService.getFormTemplatesByGuid doc for more info")
 	fun getFormTemplatesByGuid(userId: String, specialityCode: String, formTemplateGuid: String): Flow<FormTemplate>
 	fun getFormTemplatesBySpecialty(specialityCode: String, loadLayout: Boolean): Flow<FormTemplate>
 	fun getFormTemplatesByUser(userId: String, loadLayout: Boolean): Flow<FormTemplate>
-
-	suspend fun modifyFormTemplate(formTemplate: FormTemplate): FormTemplate?
 }
