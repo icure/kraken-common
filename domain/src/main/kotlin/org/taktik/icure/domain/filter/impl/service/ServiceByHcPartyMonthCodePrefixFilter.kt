88567dedee595af7b6094e45382ec9f2147bc6b9
aduchate <ad@icure.com>
2026-04-14 13:55:09 +0200

    Add missing matches implementations in filters

--- domain/src/main/kotlin/org/taktik/icure/domain/filter/impl/service/ServiceByHcPartyMonthCodePrefixFilter.kt
+++ domain/src/main/kotlin/org/taktik/icure/domain/filter/impl/service/ServiceByHcPartyMonthCodePrefixFilter.kt
@@ -7,6 +7,8 @@ import org.taktik.icure.domain.filter.AbstractFilter
 import org.taktik.icure.domain.filter.service.ServiceByHcPartyMonthCodePrefixFilter
 import org.taktik.icure.entities.base.HasEncryptionMetadata
 import org.taktik.icure.entities.embed.Service
+import org.taktik.icure.entities.embed.withEncryptionMetadata
+import org.taktik.icure.utils.FuzzyDates
 
 data class ServiceByHcPartyMonthCodePrefixFilter(
 	override val desc: String? = null,
@@ -24,6 +26,25 @@ data class ServiceByHcPartyMonthCodePrefixFilter(
 	override val requiresSecurityPrecondition: Boolean = false
 	override fun requestedDataOwnerIds(): Set<String> = setOf(healthcarePartyId)
 
-	override fun matches(item: Service, searchKeyMatcher: (String, HasEncryptionMetadata) -> Boolean): Boolean =
-		throw UnsupportedOperationException()
+	override fun matches(item: Service, searchKeyMatcher: (String, HasEncryptionMetadata) -> Boolean): Boolean {
+		if (item.endOfLife != null) return false
+		if (item.withEncryptionMetadata()?.let { searchKeyMatcher(healthcarePartyId, it) } != true) return false
+		if (!item.codes.any { codeType == it.type && it.code?.startsWith(codeCodePrefix) == true }) return false
+
+		val date = item.valueDate ?: item.openingDate
+		val dateAsInt = date?.let { if (it > FuzzyDates.MAX_FUZZY_DATE) (it / 1000000).toInt() else it.toInt() }
+		val itemYear = dateAsInt?.let { it / 10000 }?.takeIf { it in 1000..9999 }
+		val itemMonth = dateAsInt?.let { it / 100 % 100 }
+
+		if (year == null && month == null) {
+			if (itemYear != null) return false
+		} else {
+			if (itemYear != year) return false
+			if (itemMonth != month) return false
+		}
+
+		if (startValueDate != null && (date == null || !FuzzyDates.isFuzzyDateAfterOrEqual(date, startValueDate))) return false
+		if (endValueDate != null && (date == null || !FuzzyDates.isFuzzyDateBeforeOrEqual(date, endValueDate))) return false
+		return true
+	}
 }
