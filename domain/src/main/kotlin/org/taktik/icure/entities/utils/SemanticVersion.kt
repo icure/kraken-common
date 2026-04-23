387a6506606bcfda0120875a47c92672cf9f8111
Luca Tremamunno <tremaluca96@gmail.com>
2026-04-23 16:28:11 +0200

    More efficient SemanticVersion compare with lazy init

--- domain/src/main/kotlin/org/taktik/icure/entities/utils/SemanticVersion.kt
+++ domain/src/main/kotlin/org/taktik/icure/entities/utils/SemanticVersion.kt
@@ -1,36 +1,61 @@
 package org.taktik.icure.entities.utils
 
-@JvmInline
-value class SemanticVersion(val version: String) : Comparable<SemanticVersion> {
+class SemanticVersion(val version: String) : Comparable<SemanticVersion> {
+	companion object {
+		private fun parse(value: String): Parsed {
+			val thisSplit = value.split('.').also {
+				require(it.size == 3) { "Invalid version format: $value" }
+			}
+			return Parsed(
+				thisSplit[0].toInt(),
+				thisSplit[1].toInt(),
+				asIntComponentNoSuffix(thisSplit[2]),
+				getComponentSuffix(thisSplit[2]).takeIf { it.isNotEmpty() }
+			)
+		}
 
-	private fun asIntComponentNoSuffix(component: String): Int = buildString {
-		component
-			.takeWhile { it in '0'..'9' }
-			.forEach { append(it) }
-	}.toInt()
 
-	private fun getComponentSuffix(component: String): String = component.dropWhile {
-		it in '0'..'9'
+		private fun asIntComponentNoSuffix(component: String): Int =
+			component.takeWhile { it in '0'..'9' }.toInt()
+
+		private fun getComponentSuffix(component: String): String =
+			component.dropWhile { it in '0'..'9' }
+	}
+
+	private class Parsed(val major: Int, val minor: Int, val patch: Int, val suffix: String?): Comparable<Parsed> {
+		override fun compareTo(other: Parsed): Int =
+			major.compareTo(other.major).takeIf { it != 0 }
+				?: minor.compareTo(other.minor).takeIf { it != 0 }
+				?: patch.compareTo(other.patch).takeIf { it != 0 }
+				?: when {
+					suffix == null && other.suffix == null -> 0
+					suffix == null -> 1
+					other.suffix == null -> -1
+					else -> suffix.compareTo(other.suffix)
+				}
+	}
+
+	private val parsed by lazy(LazyThreadSafetyMode.PUBLICATION) {
+		parse(version)
 	}
 
 	override fun compareTo(other: SemanticVersion): Int {
-		val thisSplit = version.split('.').also {
-			require(it.size == 3) { "Invalid version format: $version" }
-		}
-		val otherSplit = other.version.split('.').also {
-			require(it.size == 3) { "Invalid version format: ${other.version}" }
-		}
-		return thisSplit[0].toInt().compareTo(otherSplit[0].toInt()).takeIf { it != 0 }
-			?: thisSplit[1].toInt().compareTo(otherSplit[1].toInt()).takeIf { it != 0 }
-			?: asIntComponentNoSuffix(thisSplit[2]).compareTo(asIntComponentNoSuffix(otherSplit[2])).takeIf { it != 0 }
-			?: getComponentSuffix(thisSplit[2]).let { thisSuffix ->
-				val otherSuffix = getComponentSuffix(otherSplit[2])
-				when {
-					thisSuffix.isEmpty() && otherSuffix.isEmpty() -> 0
-					thisSuffix.isEmpty() -> 1
-					otherSuffix.isEmpty() -> -1
-					else -> thisSuffix.compareTo(otherSuffix)
-				}
-			}
+		return this.parsed.compareTo(other.parsed)
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other !is SemanticVersion) return false
+
+		if (version != other.version) return false
+		if (parsed != other.parsed) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		var result = version.hashCode()
+		result = 31 * result + parsed.hashCode()
+		return result
 	}
 }
