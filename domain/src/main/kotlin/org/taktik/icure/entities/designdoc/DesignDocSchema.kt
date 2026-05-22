2bafb761c5e2e7ac2e321764334fc2643eec4687
Vincenzo Pierro <vcp@icure.com>
2026-05-22 10:41:07 +0200

    Refactored serialization of schema

--- domain/src/main/kotlin/org/taktik/icure/entities/designdoc/DesignDocSchema.kt
+++ domain/src/main/kotlin/org/taktik/icure/entities/designdoc/DesignDocSchema.kt
@@ -1,5 +1,7 @@
 package org.taktik.icure.entities.designdoc
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.taktik.couchdb.entity.Attachment
@@ -9,7 +11,9 @@ import org.taktik.icure.entities.embed.RevisionInfo
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class DesignDocSchema(
 	@param:JsonProperty("_rev") override val rev: String?,
+	@JsonIgnore
 	val applicationGroupId: String,
+	@JsonIgnore
 	val version: Int,
 	/**
 	 * The key of the outermost map is the entity name. In the inner map, the key is the view name and the value is
@@ -24,8 +28,36 @@ data class DesignDocSchema(
 
 	companion object {
 		private const val ID_PREFIX = "ddc"
-
 		fun idOf(applicationGroupId: String, version: Int): String = "$ID_PREFIX:$applicationGroupId:$version"
+
+		@JvmStatic
+		@JsonCreator
+		fun jsonConstructor(
+			@JsonProperty("_id") id: String,
+			@JsonProperty("_rev") rev: String?,
+			viewsByEntity: Map<String, Map<String, Int>>,
+			deletionDate: Long? = null,
+			@JsonProperty("_attachments") attachments: Map<String, Attachment>? = mapOf(),
+			@JsonProperty("_revs_info") revisionsInfo: List<RevisionInfo>? = listOf(),
+			@JsonProperty("_conflicts") conflicts: List<String>? = listOf()
+		): DesignDocSchema {
+			val (groupApplicationId, version) = id.split(":").let {
+				require(it.size == 3) { "Invalid id for schema, should have 3 components" }
+				require(it[0] == ID_PREFIX) { "Invalid id for configuration, should start with '$ID_PREFIX'" }
+				requireNotNull(it[2].toIntOrNull()) { "Invalid id for schema, version must be an int" }
+				it[1] to it[2].toInt()
+			}
+			return DesignDocSchema(
+				rev = rev,
+				applicationGroupId = groupApplicationId,
+				version = version,
+				viewsByEntity = viewsByEntity,
+				deletionDate = deletionDate,
+				attachments = attachments,
+				revisionsInfo = revisionsInfo,
+				conflicts = conflicts
+			)
+		}
 	}
 
 	@JsonProperty("_id")
