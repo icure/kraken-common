01a82fc8a447d4b1950a9f0d08d2e3aaa4042027
Vincenzo Pierro <vcp@icure.com>
2026-06-26 10:14:34 +0200

    Added code doc on LegacyField

--- dto/src/main/kotlin/org/taktik/icure/dto/annotations/filtering/FilterBeforeSdkVersion.kt
+++ dto/src/main/kotlin/org/taktik/icure/dto/annotations/filtering/FilterBeforeSdkVersion.kt
@@ -1,4 +1,8 @@
 package org.taktik.icure.dto.annotations.filtering
 
+/**
+ * Marks a field as introduced in a specific version of the SDK and the filter generator will add it to the Jackson Filter in the object mapper.
+ * This field will not be serialized if the user is using an SDK version that is strictly less than the specified [version].
+ */
 @Target(AnnotationTarget.PROPERTY)
 annotation class FilterBeforeSdkVersion(val version: String)
