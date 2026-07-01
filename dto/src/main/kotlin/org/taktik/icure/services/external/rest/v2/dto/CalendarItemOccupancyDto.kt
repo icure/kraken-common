1a651d1bf550d55b74cec18ed111163f4116fb6d
aduchate <ad@icure.com>
2026-07-01 16:02:18 +0200

    Add missing @ActiveField on new DTO

--- dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/CalendarItemOccupancyDto.kt
+++ dto/src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/CalendarItemOccupancyDto.kt
@@ -2,6 +2,7 @@ package org.taktik.icure.services.external.rest.v2.dto
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
+import org.taktik.icure.dto.annotations.filtering.ActiveField
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -13,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
  */
 data class CalendarItemOccupancyDto(
 	/** A fuzzy date-time at which the occupancy changes. */
-	val timestamp: Long,
+	@ActiveField val timestamp: Long,
 	/** The number of calendar items that are concurrently busy starting from [timestamp]. */
-	val occupancy: Long,
+	@ActiveField val occupancy: Long,
 )
