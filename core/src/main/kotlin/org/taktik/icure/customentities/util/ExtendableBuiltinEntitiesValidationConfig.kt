858906d31c8954cf1e9ef2973a35aec5259f12ff
Luca Tremamunno <tremaluca96@gmail.com>
2026-08-31 09:02:24 +0200

    Fix ExtendableBuiltinEntitiesValidationConfig

--- core/src/main/kotlin/org/taktik/icure/customentities/util/ExtendableBuiltinEntitiesValidationConfig.kt
+++ core/src/main/kotlin/org/taktik/icure/customentities/util/ExtendableBuiltinEntitiesValidationConfig.kt
@@ -1,28 +1,39 @@
-// TODO this is auto-generated but should be moved to a build folder and tied to a gradle task
 package com.icure.cardinal.customentities.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.`annotation`.Bean
 import org.springframework.context.`annotation`.Configuration
 import org.taktik.icure.entities.base.CodeStub
+import org.taktik.icure.entities.base.DataOwnerGroupLinkType
 import org.taktik.icure.entities.base.LinkQualification
+import org.taktik.icure.entities.base.ParticipantType
 import org.taktik.icure.entities.embed.Address
 import org.taktik.icure.entities.embed.AddressType
 import org.taktik.icure.entities.embed.Annotation
+import org.taktik.icure.entities.embed.AsserterType
 import org.taktik.icure.entities.embed.CareTeamMember
 import org.taktik.icure.entities.embed.CareTeamMemberType
 import org.taktik.icure.entities.embed.CareTeamMembership
 import org.taktik.icure.entities.embed.Content
+import org.taktik.icure.entities.embed.ContractChangeType
+import org.taktik.icure.entities.embed.DocumentLocation
 import org.taktik.icure.entities.embed.DocumentStatus
 import org.taktik.icure.entities.embed.DocumentType
 import org.taktik.icure.entities.embed.Episode
+import org.taktik.icure.entities.embed.FlatRateType
 import org.taktik.icure.entities.embed.Gender
+import org.taktik.icure.entities.embed.HealthcarePartyStatus
 import org.taktik.icure.entities.embed.Identifier
 import org.taktik.icure.entities.embed.Insurability
 import org.taktik.icure.entities.embed.Laterality
 import org.taktik.icure.entities.embed.Measure
 import org.taktik.icure.entities.embed.Medication
 import org.taktik.icure.entities.embed.MembershipType
+import org.taktik.icure.entities.embed.MhcSignatureType
+import org.taktik.icure.entities.embed.PartnerType
+import org.taktik.icure.entities.embed.PartnershipStatus
+import org.taktik.icure.entities.embed.PartnershipType
+import org.taktik.icure.entities.embed.PatientHealthCarePartyType
 import org.taktik.icure.entities.embed.PersonName
 import org.taktik.icure.entities.embed.PersonNameUse
 import org.taktik.icure.entities.embed.PersonalStatus
@@ -30,28 +41,42 @@ import org.taktik.icure.entities.embed.PlanOfAction
 import org.taktik.icure.entities.embed.Service
 import org.taktik.icure.entities.embed.ServiceLink
 import org.taktik.icure.entities.embed.SubContact
+import org.taktik.icure.entities.embed.SuspensionReason
 import org.taktik.icure.entities.embed.Telecom
 import org.taktik.icure.entities.embed.TelecomType
 import org.taktik.icure.entities.embed.TimeSeries
+import org.taktik.icure.entities.embed.TypedValuesType
 import org.taktik.icure.services.`external`.rest.v2.dto.base.CodeStubDto
+import org.taktik.icure.services.`external`.rest.v2.dto.base.DataOwnerGroupLinkTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.base.IdentifierDto
 import org.taktik.icure.services.`external`.rest.v2.dto.base.LinkQualificationDto
+import org.taktik.icure.services.`external`.rest.v2.dto.base.ParticipantTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.AddressDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.AddressTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.AnnotationDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.AsserterTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.CareTeamMemberDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.CareTeamMemberTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.CareTeamMembershipDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.ContentDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.ContractChangeTypeDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.DocumentLocationDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.DocumentStatusDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.DocumentTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.EpisodeDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.FlatRateTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.GenderDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.HealthcarePartyStatusDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.InsurabilityDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.LateralityDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.MeasureDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.MedicationDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.MembershipTypeDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.MhcSignatureTypeDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.PartnerTypeDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.PartnershipStatusDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.PartnershipTypeDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.PatientHealthCarePartyTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.PersonNameDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.PersonNameUseDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.PersonalStatusDto
@@ -59,9 +84,11 @@ import org.taktik.icure.services.`external`.rest.v2.dto.embed.PlanOfActionDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.ServiceDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.ServiceLinkDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.SubContactDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.SuspensionReasonDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.TelecomDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.TelecomTypeDto
 import org.taktik.icure.services.`external`.rest.v2.dto.embed.TimeSeriesDto
+import org.taktik.icure.services.`external`.rest.v2.dto.embed.TypedValuesTypeDto
 import org.taktik.icure.services.`external`.rest.v2.mapper.base.CodeStubV2Mapper
 import org.taktik.icure.services.`external`.rest.v2.mapper.base.IdentifierV2Mapper
 import org.taktik.icure.services.`external`.rest.v2.mapper.embed.AddressV2Mapper
@@ -144,6 +171,7 @@ public class ExtendableBuiltinEntitiesValidationConfig {
                 codeStubV2Mapper::map)
             addMappersForExposedBuiltinObject<IdentifierDto, Identifier>(identifierV2Mapper::map,
                 identifierV2Mapper::map)
+            addMapperForBuiltinEnum<DocumentLocationDto, DocumentLocation>()
             addMapperForBuiltinEnum<DocumentTypeDto, DocumentType>()
             addMapperForBuiltinEnum<DocumentStatusDto, DocumentStatus>()
             addMapperForBuiltinEnum<AddressTypeDto, AddressType>()
@@ -153,7 +181,20 @@ public class ExtendableBuiltinEntitiesValidationConfig {
             addMapperForBuiltinEnum<MembershipTypeDto, MembershipType>()
             addMapperForBuiltinEnum<TelecomTypeDto, TelecomType>()
             addMapperForBuiltinEnum<LateralityDto, Laterality>()
+            addMapperForBuiltinEnum<DataOwnerGroupLinkTypeDto, DataOwnerGroupLinkType>()
             addMapperForBuiltinEnum<GenderDto, Gender>()
             addMapperForBuiltinEnum<PersonalStatusDto, PersonalStatus>()
+            addMapperForBuiltinEnum<HealthcarePartyStatusDto, HealthcarePartyStatus>()
+            addMapperForBuiltinEnum<ParticipantTypeDto, ParticipantType>()
+            addMapperForBuiltinEnum<PartnershipTypeDto, PartnershipType>()
+            addMapperForBuiltinEnum<PartnershipStatusDto, PartnershipStatus>()
+            addMapperForBuiltinEnum<PartnerTypeDto, PartnerType>()
+            addMapperForBuiltinEnum<PatientHealthCarePartyTypeDto, PatientHealthCarePartyType>()
+            addMapperForBuiltinEnum<ContractChangeTypeDto, ContractChangeType>()
+            addMapperForBuiltinEnum<SuspensionReasonDto, SuspensionReason>()
+            addMapperForBuiltinEnum<MhcSignatureTypeDto, MhcSignatureType>()
+            addMapperForBuiltinEnum<FlatRateTypeDto, FlatRateType>()
+            addMapperForBuiltinEnum<TypedValuesTypeDto, TypedValuesType>()
+            addMapperForBuiltinEnum<AsserterTypeDto, AsserterType>()
         }.build()
 }
